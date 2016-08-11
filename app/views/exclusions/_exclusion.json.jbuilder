1d51431164794c8b567b95a70677c4d5063c57e2
Tim Herd <rz@eqdw.net>
2016-08-11 15:01:49 -0700

    scaffolds

---
+++ app/views/exclusions/_exclusion.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! exclusion, :id, :created_at, :updated_at
+json.url exclusion_url(exclusion, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
-  resources :exclusions
-  resources :pairs
   resources :teams
   resources :people
+  resources :exclusions
+  resources :pairs
+  resources :pairs
+  root to: "pairs#dashboard"
+  resources :exclusions
+  resources :teams
+  resources :people
+
+  resources :pairs do
+    get :dashboard
+  end
 end
